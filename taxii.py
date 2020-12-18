253ef2be83c0af0e8050cbb66ab2ad0ac00be7da
ARebel <accidentalrebel@gmail.com>
2020-12-18 20:41:40 +0800

    For testing purposes can now load from local.

--- taxii.py
+++ taxii.py
@@ -1,22 +1,26 @@
 from taxii2client.v20 import Server, Collection
-from stix2 import Filter, TAXIICollectionSource
+from stix2 import Filter, TAXIICollectionSource, FileSystemSource
 
-collections = {
-    'enterprise_attack': '95ecc380-afe9-11e4-9b6c-751b66dd541e',
-    'pre_attack': '062767bd-02d2-4b72-84ba-56caef0f8658',
-    'mobile_attack': '2f669986-b40b-4423-b720-4396ca6a462b',
-    'ics_attack': '02c3ef24-9cd4-48f3-a99f-b74ce24f1d34'
+IS_ONLINE = False
+
+if IS_ONLINE:
+    collections = {
+        'enterprise_attack': '95ecc380-afe9-11e4-9b6c-751b66dd541e',
+        'pre_attack': '062767bd-02d2-4b72-84ba-56caef0f8658',
+        'mobile_attack': '2f669986-b40b-4423-b720-4396ca6a462b',
+        'ics_attack': '02c3ef24-9cd4-48f3-a99f-b74ce24f1d34'
+    }
     
-}
+    server = Server('https://cti-taxii.mitre.org/taxii/')
+    api_root = server.api_roots[0]
 
-server = Server('https://cti-taxii.mitre.org/taxii/')
-api_root = server.api_roots[0]
-
-print('Getting collection...')
-collection = Collection(f'https://cti-taxii.mitre.org/stix/collections/{collections["enterprise_attack"]}/')
-print('Collection received.')
-src = TAXIICollectionSource(collection)
-print('src created.')
+    print('Getting collection...')
+    collection = Collection(f'https://cti-taxii.mitre.org/stix/collections/{collections["enterprise_attack"]}/')
+    print('Collection received.')
+    src = TAXIICollectionSource(collection)
+    print('src created.')
+else:
+    src = FileSystemSource('./cti/enterprise-attack')
 
 # Print name and ID of all ATT&CK domains available as collections
 # for collection in api_root.collections:
